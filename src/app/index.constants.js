2196dfc350601882d743c783929a8bb0cbf961de
loljoho <joho@joho.io>
2015-11-18 11:20:53 -0500

    yo-gulp-angular

---
+++ src/app/index.constants.js
@@ -0,0 +1,10 @@
+/* global malarkey:false, moment:false */
+(function() {
+  'use strict';
+
+  angular
+    .module('poppygp')
+    .constant('malarkey', malarkey)
+    .constant('moment', moment);
+
+})();
