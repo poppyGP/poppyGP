93ad7efa88da0f76f830e5ee7b298757bb42683b
loljoho <jho1290@smail.raritanval.edu>
2015-11-19 22:33:24 -0500

    Layout and Player

--- src/app/core/core.config.js
+++ src/app/core/core.config.js
@@ -6,16 +6,41 @@
     .config(config);
 
   /** @ngInject */
-  function config($logProvider, toastrConfig) {
+  function config($logProvider, $sceDelegateProvider, toastrConfig) {
+
+
+    // Whitelist Embed URLs
+    // ------------------------------
+
+    $sceDelegateProvider.resourceUrlWhitelist([
+      'self',
+      'http://www.ustream.tv/embed/**',
+      'https://www.ustream.tv/embed/**'
+    ]);
+
+    // ------------------------------
+
+
     // Enable log
+    // ------------------------------
+
     $logProvider.debugEnabled(true);
 
+    // ------------------------------
+
+
     // Set options third-party lib
+    // ------------------------------
+
     toastrConfig.allowHtml = true;
     toastrConfig.timeOut = 3000;
     toastrConfig.positionClass = 'toast-top-right';
     toastrConfig.preventDuplicates = true;
     toastrConfig.progressBar = true;
+
+    // ------------------------------
+
+
   }
 
 })();
