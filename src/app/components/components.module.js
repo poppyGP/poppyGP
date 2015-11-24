a00bb9b7ee6bbc49c185fa5dac6b00a4f28cd7e9
loljoho <jho1290@smail.raritanval.edu>
2015-11-24 11:28:55 -0500

    WIP - Layout with Flexbox

--- src/app/components/components.module.js
+++ src/app/components/components.module.js
@@ -2,6 +2,6 @@
   'use strict';
 
   angular
-    .module('poppygp.components', ['ngAnimate']);
+    .module('poppygp.components', []);
 
 })();
--- src/app/core/core.route.js
+++ src/app/core/core.route.js
@@ -13,6 +13,12 @@
         templateUrl: 'app/layout/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('test', {
+        url: '/test',
+        templateUrl: 'app/layout/split.html',
+        controller: 'MainController',
+        controllerAs: 'main'
       });
 
     $urlRouterProvider.otherwise('/');
